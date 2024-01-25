59736083d13de78ab67ed3ebf07c7b6d27bfde31
mochell <momme.hell@googlemail.com>
2024-01-25 09:13:26 -0700

    bug fixes in tests

--- tests/T04_2D_box_diagonal_BC.jl
+++ tests/T04_2D_box_diagonal_BC.jl
@@ -1,5 +1,8 @@
 ENV["JULIA_INCREMENTAL_COMPILE"] = true
 
+using Pkg
+Pkg.activate(".")
+
 #using ModelingToolkit, DifferentialEquations
 #using ModelingToolkit#: @register_symbolic
 #using Plots
@@ -27,6 +30,8 @@ using GLMakie
 
 using PiCLES.Operators.core_2D: GetGroupVelocity, speed
 using PiCLES.Plotting.movie: init_movie_2D_box_plot
+
+using Revise
 
 # debugging:
 #using ProfileView
@@ -167,159 +172,3 @@ initialize_simulation!(wave_simulation)
 istate = wave_simulation.store.store[end];
 p1 = plt.heatmap(gn.x / 1e3, gn.y / 1e3, istate[:, :, 1])
 
-
-# %%
-# show all Failed particles
-using PiCLES.Utils: ParticleTools
-
-Revise.retry()
-DD = ParticleTools.ParticleToDataframe(wave_simulation.model.FailedCollection)
-
-DD_stats = ParticleTools.ParticleStatsToDataframe(wave_simulation.model.FailedCollection)
-
-#sum(DD_stats.boundary)
-
-function plot_state_and_error_points(wave_simulation, FailedTable)
-    plt.plot()
-
-    p1 = plt.heatmap(gn.x / 1e3, gn.y / 1e3, transpose(wave_simulation.model.State[:, :, 1]))
-
-    for xy in FailedTable.position_xy
-        x = xy[1] / 1e3
-        y = xy[2] / 1e3
-        plt.scatter!((x, y), color="red", label="wind")
-    end
-
-    plt.plot!(legend=:none, title="c_g", ylabel="cg m/s", xlabel="position index x") |> display
-end
-
-plot_state_and_error_points(wave_simulation, DD_stats)
-# %%
-
-## testing single ODES
-PF = wave_simulation.model.FailedCollection[end]
-PF.Particle
-#PF.Particle.ODEIntegrator.u = [-15.441984291167334, 0.006707651986269529, 0.003889144130029894, 4000.0, 2333.3333333333335]
-PF.Particle.ODEIntegrator.u
-PF.Particle.ODEIntegrator.t
-PF.Particle.ODEIntegrator.sol.t
-PF.Particle.ODEIntegrator.sol.u
-PF.Particle.ODEIntegrator.sol.prob.u0
-
-using PiCLES.Operators.core_2D: Get_u_FromShared, GetVariablesAtVertex
-S_state = Get_u_FromShared(PF.Particle, wave_simulation.model.MovieState)
-ui = GetVariablesAtVertex(S_state, PF.Particle.position_xy[1], PF.Particle.position_xy[2])
-
-usel = PF.Particle.ODEIntegrator.sol.prob.u0
-t_sel = PF.Particle.ODEIntegrator.sol.u
-index, weight = PIC.compute_weights_and_index(grid, usel[4], usel[5])
-
-using PiCLES: ParticleInCell as PIC
-index, weight = PIC.compute_weights_and_index(grid, PF.Particle.ODEIntegrator.u[4], PF.Particle.ODEIntegrator.u[5])
-
-
-# try manual solution with the same initial conditions
-using DifferentialEquations
-
-#PF.Particle.ODEIntegrator.u = ui
-ui2 = copy(PF.Particle.ODEIntegrator.sol.prob.u0)
-reinit!(PF.Particle.ODEIntegrator, ui2, erase_sol=false, reset_dt=false, reinit_cache=true)
-u_modified!(PF.Particle.ODEIntegrator, true)
-#auto_dt_reset!(PF.Particle.ODEIntegrator)
-PF.Particle.ODEIntegrator.t
-# set_t!(PF.Particle.ODEIntegrator, PF.Particle.ODEIntegrator.sol.t[end])
-
-wind_tuple = winds.u(PF.Particle.position_xy[1], PF.Particle.position_xy[2], PF.Particle.ODEIntegrator.t), winds.v(PF.Particle.position_xy[1], PF.Particle.position_xy[2], PF.Particle.ODEIntegrator.t)
-step!(PF.Particle.ODEIntegrator, DT, true)
-PF.Particle.ODEIntegrator.u
-
-
-function set_u_and_t!(integrator, u_new, t_new)
-    integrator.u = u_new
-    integrator.t = t_new
-end
-
-#5min
-DT = 5min#wave_simulation.model.ODEsettings.timestep
-ui2
-set_u_and_t!(PF.Particle.ODEIntegrator, ui2, 0.0)
-auto_dt_reset!(PF.Particle.ODEIntegrator)
-step!(PF.Particle.ODEIntegrator, DT, true)
-PF.Particle.ODEIntegrator.u
-
-#index, weight = PIC.compute_weights_and_index(grid, PF.Particle.ODEIntegrator.u[4], PF.Particle.ODEIntegrator.u[5])
-
-#PF.Particle.ODEIntegrator.sol
-#wave_simulation.model.winds.u()
-
-# %%
-#PF.Particle.boundary
-Revise.retry()
-
-istate = wave_simulation.store.store[end];
-#istate = wave_simulation.store.store[5];
-#wave_simulation.model.ParticleCollection[105]
-
-cg = GetGroupVelocity(istate)
-p1 = plt.heatmap(gn.x / 1e3, gn.y / 1e3, istate[:, :, 1])
-
-# %%
-p1 = plt.heatmap(gn.x / 1e3, gn.y / 1e3, cg.c_x)
-
-# %%
-#pcolormesh in Julia 
-# gn = TwoDGridNotes(grid);
-
-# gridmesh = [(i,j) for i in gn.x, j in gn.y];
-# mesh.x = [i for i in gn.x, j in gn.y];
-# mesh.y = [j for i in gn.x, j in gn.y];
-# mesh.x = mesh.x[1:3:end, 1:3:end];
-# mesh.y = mesh.y[1:3:end, 1:3:end];
-
-# %%
-# %%
-
-p1 = plt.heatmap(gn.x / 1e3, gn.y / 1e3, wave_simulation.model.State[:, :, 2])
-
-# plt.quiver!(p1, mesh.x / 1e3, mesh.y / 1e3, quiver=(wave_simulation.model.State[1:3:end, 1:3:end, 2] * 20, wave_simulation.model.State[1:3:end, 1:3:end, 3] * 20), color=:red, scale_unit=:data, label="wind")
-# # scale_unit options: :width, :height, :x, :y, :xy, :data
-
-plt.contour(p1, gn.x / 1e3, gn.y / 1e3, u.(mesh.x, mesh.y, wave_simulation.model.clock.time), color="white")
-
-# # heatmap(wave_simulation.model.State[:, :, 2])
-# # heatmap(wave_simulation.model.State[:, :, 3])
-
-
-#sqrt( wave_simulation.model.winds.v.(mesh.x, mesh.y, 0.0)^2 + wave_simulation.model.winds.u.(mesh.x, mesh.y, 0.0)^2)
-
-# %%
-Revise.retry()
-wave_model = WaveGrowthModels2D.WaveGrowth2D(; grid=grid,
-    winds=winds,
-    ODEsys=particle_system,
-    ODEsets=ODE_settings,  # ODE_settings
-    ODEinit_type="wind_sea",  # default_ODE_parameters
-    periodic_boundary=false,
-    boundary_type="same",
-    #minimal_particle=FetchRelations.MinimalParticle(U10, V10, DT),
-    movie=true)
-
-wave_simulation = Simulation(wave_model, Δt=5minutes, stop_time=4hours)#1hours)
-initialize_simulation!(wave_simulation)
-
-#reset_simulation!(wave_simulation, particle_initials=copy(wave_model.ODEdefaults))
-
-fig, n = init_movie_2D_box_plot(wave_simulation, name_string="Rotating Stationary Winds")
-
-#wave_simulation.stop_time += 1hour
-N = 250
-record(fig, save_path * "T02_2D_totating_winds_nonper.gif", 1:N, framerate=10) do i
-    @info "Plotting frame $i of $N..."
-    #@time for _ = 1:10
-    #run!(wave_simulation, store=false)
-    @info wave_simulation.model.clock
-    movie_time_step!(wave_simulation.model, wave_simulation.Δt)
-    #coupled_time_step!(ocean_simulation, ice_simulation)
-    #end
-    n[] = 1
-end
